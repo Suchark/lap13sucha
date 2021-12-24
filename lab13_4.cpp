2649565de032ce04e3caae3a3a0431acc4d1cb0f
Shuchanuch Rinkum <Suchanuch28@gmail.com>
2021-12-24 18:50:31 +0700

    lab 13 completed

--- lab13_4.cpp
+++ lab13_4.cpp
@@ -2,6 +2,10 @@
 
 using namespace std;
 
+template <typename currentType>
+void mySwap(currentType &,currentType &);
+
+
 int main(){
 	int x, y;
 	string a, b;
@@ -30,3 +34,10 @@ int main(){
 	return 0;
 }
 
+template <typename currentType>
+void mySwap(currentType &x,currentType &y)
+{
+    currentType temp = x;
+    x = y;
+    y = temp;
+}
